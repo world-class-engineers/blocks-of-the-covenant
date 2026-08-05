6848144ed7cd2988815156faaeae1a379d579d75
Joe Skeen <joeskeen@outlook.com>
2026-08-05 12:52:46 -0600

    add todo for water to blood plague

--- scripts/plagues/water-to-blood.plague.ts
+++ scripts/plagues/water-to-blood.plague.ts
@@ -1,4 +1,4 @@
-import { filter, interval, map, switchMap, takeUntil, tap } from 'rxjs';
+import { interval, map, switchMap, takeUntil, tap } from 'rxjs';
 import { BasePlague } from './base-plague';
 
 const POLLING_INTERVAL_MS = 1000;
@@ -24,12 +24,23 @@ export class WaterToBloodPlague extends BasePlague {
           interval(POLLING_INTERVAL_MS).pipe(
             takeUntil(this.deactivated$),
             map(() => this.world.getAllPlayers()),
+            // TODO:
+            // map players to the volume surrounding them
+            // check for water blocks in that volume
+            // and replace them with blood blocks
             tap((players) =>
               console.log(players.map((p) => p.name).join(', ')),
             ),
           ),
         ),
       )
+      .subscribe();
+
+    this.deactivated$
+      .pipe
+      // map event to all blood blocks in the world
+      // replace them with water blocks
+      ()
       .subscribe();
   }
 }
